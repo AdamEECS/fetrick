f2d96a55a219868c88983a5694ca8efd9bb710db
suzumiya <jrgwyz@163.com>
2017-10-20 19:52:32 +0800

    canvas

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route("/canvas")
+def canvas():
+    return render_template('canvas.html')
+
 if __name__ == "__main__":
     app.run()
